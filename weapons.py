1a94c57bf010b532b03ff6a1a4dcc01366a8a6f4
hadino <itshadino@gmail.com>
2026-03-19 16:54:23 +0530

    new file:   weapons.json 	new file:   weapons.py

---
+++ weapons.py
@@ -0,0 +1,24 @@
+import json
+
+with open('weapons.json') as weapon_file:
+    weapons = json.load(weapon_file)
+
+weapons_instances = []
+
+class Weapon:
+    def __init__(self,name,type,traits,lethality):
+        self.name = name
+        self.type = type
+        self.traits = traits
+        self.letahlity = lethality
+    def __repr__(self): 
+        return f"{self.name}"
+
+for k in weapons:
+    new_weapon = Weapon(
+        name = k['name'],
+        type = k['type'],
+        traits = k['traits'],
+        lethality = k['lethality']
+    )
+    weapons_instances.append(new_weapon)
