04fec6693987e7c257e4cba7f888cb75b7553063
hadino <itshadino@gmail.com>
2026-03-19 15:14:10 +0530

    modified:   tributes.json 	modified:   tributes.py

--- tributes.py
+++ tributes.py
@@ -6,10 +6,11 @@ with open('tributes.json') as tribute_file:
 tributes_instances = []
 
 class Tribute:
-    def __init__(self,name,traits,status,alive):
+    def __init__(self,name,traits,status,hp,alive):
         self.name = name
         self.traits = traits
         self.status = status
+        self.hp = hp
         self.alive = alive
     def __repr__(self): 
         return f"{self.name}"
@@ -18,7 +19,8 @@ for i in tributes:
     new_tribute = Tribute(
         name=i['name'], 
         traits=i['traits'], 
-        status=i['status'], 
+        status=i['status'],
+        hp = i['hp'], 
         alive=i['alive']
     )
     tributes_instances.append(new_tribute)
